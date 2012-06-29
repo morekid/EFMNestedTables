311d2bfb87807e7ecf08d21d56ff98c006d322b3
morekid <daniele.dematteis@gmail.com>
2012-06-29 15:43:26 +0100

    default images update + files update

--- EFMNestedTablesExample/AppDelegate.h
+++ EFMNestedTablesExample/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  nestedTables
+//  EFMNestedTablesExample
 //
 //  Created by Daniele De Matteis on 21/05/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Daniele De Matteis. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
